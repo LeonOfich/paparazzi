28856557e6a2c1cf4f5820f5dc4954a16a536b12
LeonOfich <lmcsijbers@gmail.com>
2018-02-27 16:02:35 +0100

    commit test

---
+++ sw/airborne/modules/sinus_input/sinus_input.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) LeonSijbers
+ *
+ * This file is part of paparazzi
+ *
+ */
+/**
+ * @file "modules/sinus_input/sinus_input.c"
+ * @author LeonSijbers
+ * Creates a sinus input of varying frequnecy
+ */
+
+#include "modules/sinus_input/sinus_input.h"
+
+// void init_sinus_input() {}
+// void periodic_sinus_input() {}
+
+
---
+++ sw/airborne/modules/sinus_input/sinus_input.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) LeonSijbers
+ *
+ * This file is part of paparazzi
+ *
+ */
+/**
+ * @file "modules/sinus_input/sinus_input.h"
+ * @author LeonSijbers
+ * Creates a sinus input of varying frequnecy
+ */
+
+#ifndef SINUS_INPUT_H
+#define SINUS_INPUT_H
+
+// extern void init_sinus_input();
+// extern void periodic_sinus_input();
+
+#endif
+
